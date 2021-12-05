8307d44d5670ed0d2f747cb9242a53efc04110ad
채오이 <41178045+chae52@users.noreply.github.com>
2021-12-05 18:17:21 +0900

    Update 최대공약수와 최소공배수.py

--- 최대공약수와 최소공배수.py
+++ 최대공약수와 최소공배수.py
@@ -1,5 +1,13 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12940
 
+# 라이브러리 사용
 import math
 def solution(n, m):
     return [math.gcd(n,m),int(n*m/math.gcd(n,m))]
+
+#라이브러리 미사용
+def solution(n, m):
+    for i in range(1,m+1):
+        if n%i==0 and m%i==0:
+            a=i
+    return [a,int(n*m/a)]
