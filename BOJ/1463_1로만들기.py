96f18dcca7f6f260f650c4881dffd7bdd05e473b
채오이 <41178045+chae52@users.noreply.github.com>
2022-08-01 01:29:02 +0900

    Update 1463_1로만들기.py

--- BOJ/1463_1로만들기.py
+++ BOJ/1463_1로만들기.py
@@ -1,3 +1,5 @@
+# top down : 시간초과
+
 import sys
 sys.setrecursionlimit(10**6)
 n=int(input())
@@ -22,3 +24,20 @@ def makeOne(n):
     return d[n]
 d=[0]*(n+1)
 print(makeOne(n))
+
+# bottom up
+n=int(input())
+d=[0]*(n+1)
+d[1]=0
+
+for i in range(2,n+1):
+    # print("**********",i)
+    d[i]=d[i-1]+1
+    # print(f"-1 d[i] = {d[i]}")
+    if i%3==0:
+        d[i]=min(d[i], d[i//3]+1)
+        # print(f"%3 d[i] = {d[i]}")
+
+    if i%2==0:
+        d[i]=min(d[i], d[i//2]+1)
+print(d[n])
